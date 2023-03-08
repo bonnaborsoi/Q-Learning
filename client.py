a7597b2f9b01f1865cc47d1b661803c105e2287f
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2023-03-08 09:21:50 -0300

    Refactoring variable names

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
 import numpy as np
 import random
-import threading
-import time
-import socket
 from connection import connect
 from connection import get_state_reward
 import os
 
+# Initializing the Q-table (96 states x 3 actions)
+# 96 states: 24 platforms x 4 directions. 3 actions: left, right and jump
 def load_matrix():
     matrix = []
     with open(f'{os.getcwd()}/resultado.txt') as file:
@@ -17,75 +16,79 @@ def load_matrix():
             matrix.append(values)
     return matrix
 
+# Save matrix
 def save_matrix(Q_table):
     with open(f'{os.getcwd()}/resultado.txt', 'w') as file:
         txt = ''
-        for estado in Q_table:
-            txt += f'{round(estado[0], 6)} {round(estado[1], 6)} {round(estado[2], 6)}\n'
+        for state in Q_table:
+            txt += f'{round(state[0], 6)} {round(state[1], 6)} {round(state[2], 6)}\n'
         file.write(txt)
 
-# Definindo o número de estados e ações
-num_estados = 96 # 24 x 4
-num_acoes = 3
-
-# Initializing the Q-table by all zeros
-#Q_table = np.zeros((num_estados, num_acoes)) 
+# Construct the Q-table 
 Q_table = load_matrix()
 
 # Define the hyperparameters
 alpha = 0.6
 gamma = 0.9
 epsilon = 0.35
-decaimento_epsilon = 0.001
-min_epsilon = 0.01
+
+#epsilon_decay = 0.001
+#min_epsilon = 0.01
 victories = 0
 
-recompensa_por_episodio = []
+reward_per_episode = []
 
-s = connect(2037) # Conecta-se ao servidor local do jogo
+# Connects to the game's local server
+s = connect(2037) 
 
-for episodio in range (1,100001):
-    estado = 0b0000000 # Estado inicial
-    estado = int(estado)
+# Number of episodes = 100000
+for episode in range (1,100001):
+    state = 0b0000000 # Initial state
+    state = int(state)
     done = False
-    recompensa_total = 0
+    total_reward = 0
 
     while not done:
         
         if random.uniform(0, 1) < epsilon:
-            action = random.randint(0,2) # Seleciona uma ação aleatória
+            action = random.randint(0,2) # Select a random action
         else:
-            action = np.argmax(Q_table[estado]) # Exploit learned values
+            action = np.argmax(Q_table[state]) # Exploit learned values
         if action == 0:
             act = "left"
         elif action == 1:
             act = "right"
         elif action == 2:
             act = "jump"
-        
-        old_value = Q_table[estado][action]
-        prox_estado, recompensa = get_state_reward(s , act)
-        prox_estado = int(prox_estado, 2) # Converte para inteiro
-        next_max = np.max(Q_table[prox_estado])
 
-        new_value = (1 - alpha) * old_value + alpha * (recompensa + gamma * next_max)
-        Q_table[estado][action] = new_value
-        recompensa_total = recompensa_total + recompensa
+        old_value = Q_table[state][action]
+        # Get the next state and reward
+        next_state, reward = get_state_reward(s , act)
 
-        estado = prox_estado
+        next_state = int(next_state, 2) # Convert to integer
+        next_max = np.max(Q_table[next_state])
+
+        # Bellman Optimality Equation
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        Q_table[state][action] = new_value
+        total_reward = total_reward + reward
+
+        # Update actual state
+        state = next_state
         save_matrix(Q_table)
 
-        if recompensa == -100 or recompensa == 300:
+        # It's done if the agent falls or reachs the goal
+        if reward == -100 or reward == 300:
             done = True
             break
 
-    # epsilon = max(min_epsilon, epsilon*np.exp(-decaimento_epsilon*episodio))
-    if recompensa == 300:
-        recompensa_total = 700 - recompensa_total
+    # Update the decay
+    # epsilon = max(min_epsilon, epsilon*np.exp(-epsilon_decay*episode))
+
+    # Total reward when the agents reachs the goal and count number of victories
+    if reward == 300:
+        total_reward = 700 - total_reward
         victories = victories + 1
-    recompensa_por_episodio.append(recompensa_total)
-    print(f'episodio: {episodio}')
-    print(f'vitórias: {victories}')
-    # print(f'epsilon: {epsilon}')
-    print(f'recompensa do episodio: {recompensa_total}')
-    print(f'{recompensa_por_episodio}')
+
+    # Stores the total reward every time the agent falls or reachs the goal 
+    reward_per_episode.append(total_reward)
